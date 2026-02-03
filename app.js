8e2193869b947b8117b975ab3f54888162c9bbab
nazaningholami <nazaningholami670@gmail.com>
2026-02-03 22:09:54 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const h1 = document.getElementById("h1");
+const btn = document.getElementById("btn");
+const btn2 = document.getElementById("btn2");
+btn.addEventListener("click", ()=>{
+    h1.innerHTML = "سلام صبح شما بخیر⭐"
+});
+btn2.addEventListener("click", ()=>{
+h1.innerHTML = ""
+})
